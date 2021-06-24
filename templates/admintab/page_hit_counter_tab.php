90f37de0ffba9d8e9dd67feed682f2a1c37a2215
Sumit Bhardwaj <sumeetbhardwaj90@gmail.com>
2021-06-24 10:09:51 +0530

    new update

--- templates/admintab/page_hit_counter_tab.php
+++ templates/admintab/page_hit_counter_tab.php
@@ -13,16 +13,16 @@
 		<?php
 		global $wpdb;
 		$limit = 5;
-		if(isset($_GET['page_no'])){
-		  $page = $_GET['page_no'];
+		if(isset( $_GET['page_no'] )){
+		  $page = esc_attr( $_GET['page_no'] );
 		}else{
 		  $page = 1;
 		}
 		$offset = ($page - 1) * $limit;
 		 $table_name = $wpdb->prefix .'wptech_visitor_counter';
-		 if(isset($_POST['show_date'])){
-			 $date_from = $_POST['date_from'];
-			 $date_to = $_POST['date_to'];
+		 if(isset( $_POST['show_date'] )){
+			 $date_from = esc_attr( $_POST['date_from'] );
+			 $date_to = esc_attr( $_POST['date_to'] );
 			 $get_page_visitor_data_show = $wpdb->get_results("SELECT * FROM $table_name WHERE visitor_time BETWEEN '{$date_from}' AND '{$date_to}' ORDER BY visitor_time DESC ");
 			}else{
 			 $get_page_visitor_data_show = $wpdb->get_results("SELECT * FROM $table_name ORDER BY visitor DESC LIMIT {$offset},{$limit}" );
@@ -31,14 +31,14 @@
 		?>
 		<tr>
 			<td><?php echo $offset + 1 ;?></td>
-			<td><?php echo $page_visitor_data->page_id ;?></td>
+			<td><?php echo esc_attr( $page_visitor_data->page_id ) ;?></td>
 			<td>
 			<a href="<?php echo get_permalink( $page_visitor_data->page_id ) ?>">
 			<?php echo strtoupper( get_the_title( $page_visitor_data->page_id  ) ) ;?></a>
 			</td>
-			<td><?php echo $page_visitor_data->visitor ;?></td>
-			<td><?php echo $page_visitor_data->visitor_time ;?></td>
-			<td><?php echo $page_visitor_data->ip_address;?></td>
+			<td><?php echo esc_attr( $page_visitor_data->visitor ) ;?></td>
+			<td><?php echo esc_attr( $page_visitor_data->visitor_time );?></td>
+			<td><?php echo esc_attr( $page_visitor_data->ip_address );?></td>
 		  </tr>
 		  <?php $offset ++; } ?>
 			</table>
